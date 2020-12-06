dd3395c4e97b5b32d08f0d4bb96a28b344cae6ee
Eugene Lykhovyd <lykhovyd@gmail.com>
2020-12-06 05:42:51 +0000

    add user via cookies

--- js2/wombet/src/App.js
+++ js2/wombet/src/App.js
@@ -4,12 +4,15 @@ import Gameboard from './Gameboard';
 import Scoreboard from './Scoreboard';
 import AddGame from './Addgame';
 import Wombar from './Wombar';
+import { CookiesProvider } from 'react-cookie';
 
 class App extends Component {
   render() {
     return (
     <Router basename="/wombet">
+     <CookiesProvider>
       <Wombar />
+     </CookiesProvider>
       <Switch>
         <Route exact path="/">
           <Gameboard />
--- js2/wombet/src/Wombar.js
+++ js2/wombet/src/Wombar.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from 'prop-types';
+
 import { NavLink } from "react-router-dom";
 
+const COOKIEUSER='wombetuser';
 
-export default function Wombar() {
+
+class Wombar extends React.Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+	constructor(props) {
+		super(props);
+    const { cookies } = this.props;
+		this.state = { user: cookies.get(COOKIEUSER), isLoggedIn: false } ;
+    this.handleLogin = this.handleLogin.bind(this);
+	}
+
+  handleLogin(event) {
+    //const form = event.currentTarget;
+    event.preventDefault(); // do not reload the page?
+    event.stopPropagation();
+    console.log('User:');
+    console.log(this.state.user);
+    if (this.state.user && this.state.user.length > 0)
+    {
+      const { cookies } = this.props;
+      cookies.set(COOKIEUSER, this.state.user);
+      this.setState ({ isLoggedIn : true });
+    }
+  }
+
+  render() {
+  const loginPart = this.state.isLoggedIn ? <p>Logged as <b>{this.state.user}</b></p> : 
+        <Form inline onSubmit={this.handleLogin}>
+          <Form.Control type="text" placeholder="username" className="mr-sm-2" value={this.state.user} onChange={e => this.setState({ user: e.target.value })}/>
+          <Button type="submit" variant="outline-success">Login</Button>
+        </Form>;
+
   // use as NavLink with Nav.Link to work correctly with Router
   return (
     <Navbar bg="light" expand="lg">
@@ -21,7 +59,11 @@ export default function Wombar() {
           <Nav.Link as={NavLink} to="/scoreboard">Scoreboard</Nav.Link>
           <Nav.Link as={NavLink} to="/newgame">New Game</Nav.Link>
         </Nav>
+        {loginPart}
       </Navbar.Collapse>
     </Navbar>
   );
+  }
 }
+
+export default withCookies(Wombar);
